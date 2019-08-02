a49148e6694882bed98184c5f04f1cccc7974a2c
[thecubix] <[timon.forrer@gmail.com]>
2019-08-02 18:05:24 +0200

    Added styling to slice

--- src/components/slices/musicians.jsx
+++ src/components/slices/musicians.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
 
+import Container from '../container'
+import Grid from '../grid'
+import Stack from '../stack'
+import Flash from '../icons/flash'
+
+import styles from './musicians.module.scss'
+
 const Musicians = (props) => {
   const musicians = props.fields
   const title = props.primary.title
   return (
-    <>
-      <h2>{title}</h2>
-      {musicians.map(m => (
-        <>
-          <img src={m.musician_image.url} alt={m.musician_image.alt} />
-          <h3>{m.musician_name}</h3>
-          <p>{m.musician_function}</p>
-        </>
-      ))}
-    </>
+    <Container>
+      <Stack>
+        <h2>{title}</h2>
+        <Grid dense>
+          {musicians.map(m => (
+            <div>
+              <img src={m.musician_image.url} alt={m.musician_image.alt} />
+              <div className={styles.musicianInfo}>
+                <h3>{m.musician_name}</h3>
+                <p className="uppercase"><Flash />{m.musician_function}</p>
+              </div>
+            </div>
+          ))}
+        </Grid>
+      </Stack>
+    </Container>
   )
 }
 
