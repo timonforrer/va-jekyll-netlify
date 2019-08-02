1ecd8f2eadbf3cc94fa3d573afdb9ed48764a4a9
[thecubix] <[timon.forrer@gmail.com]>
2019-08-02 18:03:58 +0200

    Added stack component

---
+++ src/components/stack.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import styles from "./stack.module.scss"
+
+const Stack = (props) => {
+  const Tag = props.tag || 'div'
+  return (
+    <Tag className={
+      props.dense ? styles.stackDense : 
+      props.extended ? styles.stackExtended : 
+      styles.stackRegular
+    }>
+      {props.children}
+    </Tag>)
+}
+
+export default Stack
