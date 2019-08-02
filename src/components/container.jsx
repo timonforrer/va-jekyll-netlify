ddbe2b15722de5aac9b1a0ec227810934934b672
[thecubix] <[timon.forrer@gmail.com]>
2019-08-02 18:03:41 +0200

    Added container component

---
+++ src/components/container.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import styles from './container.module.scss'
+
+const Container = (props) => {
+  const Tag = props.tag || 'div'
+  return <Tag className={`${styles.container} ${props.dense ? styles.containerDense : ''}`}>{props.children}</Tag>
+}
+
+export default Container
