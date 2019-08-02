4521ead67192e8273583a61f2dc5ba034c126c28
[thecubix] <[timon.forrer@gmail.com]>
2019-08-03 01:26:02 +0200

    Added reverse column functionality

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -4,12 +4,12 @@ import styles from './grid.module.scss'
 
 const Grid = (props) => {
   return (
-    <div className={
-      `${
-        props.dense ? styles.fukolGridDense :
-        props.extended ? styles.fukolGridExtended : ''
-      } ${styles.fukolGrid}`
-    }>
+    <div className={[
+      props.dense ? styles.fukolGridDense :
+      props.extended ? styles.fukolGridExtended : '',
+      props.reverse ? styles.fukolGridReverse : '',
+      styles.fukolGrid
+    ].join(' ')}>
       {props.children}
     </div>
   )
