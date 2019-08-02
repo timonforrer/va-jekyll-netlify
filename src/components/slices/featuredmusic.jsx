d2084343fe2086507c82f4afcf916099d234b4a4
[thecubix] <[timon.forrer@gmail.com]>
2019-08-02 18:05:13 +0200

    Added container to slice

--- src/components/slices/featuredmusic.jsx
+++ src/components/slices/featuredmusic.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { RichText } from 'prismic-reactjs'
 
+import Container from '../container'
+
 const FeaturedMusic = (props) => {
   let data = props.primary.link
   return (
-    <div>
+    <Container>
       <h2>{RichText.asText(data.title)}</h2>
-    </div>
+    </Container>
   )
 }
 
