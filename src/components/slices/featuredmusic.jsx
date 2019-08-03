cffe0abd745e920dfcbd6fa6f02ecc34bd8435e8
[thecubix] <[timon.forrer@gmail.com]>
2019-08-03 17:05:45 +0200

    Finalised desktop version of featuredmusic slice

--- src/components/slices/featuredmusic.jsx
+++ src/components/slices/featuredmusic.jsx
@@ -1,14 +1,53 @@
 import React from 'react'
 import { RichText } from 'prismic-reactjs'
 
+import ButtonLink from '../buttonlink'
 import Container from '../container'
+import Heading from '../heading'
+import Picture from '../picture'
+import Stack from '../stack'
+
+import styles from './featuredmusic.module.scss'
 
 const FeaturedMusic = (props) => {
   let data = props.primary.link
   return (
-    <Container>
-      <h2>{RichText.asText(data.title)}</h2>
-    </Container>
+    <section className={styles.musicSection}>
+      <Container>
+        <Stack>
+          <div>
+            <p className={['uppercase', styles.musicSection__secondaryInfo].join(' ')}>Neue Musik!</p>
+            <Heading>{RichText.asText(data.title)}</Heading>
+          </div>
+          <div>
+            <div className={styles.grid}>
+              <div>
+                <Picture {...data.cover_photo} />
+              </div>
+              <div dangerouslySetInnerHTML={{ __html: data.spotify_embed.html }} />
+            </div>
+          </div>
+          <Stack dense>
+            <h3>Erhältlich auf</h3>
+            <div>
+              <div className={styles.buttons}>
+                {data.providers.map((provider, index) => (
+                  <ButtonLink to={provider.link.url} key={index}>{provider.provider_name}</ButtonLink>
+                ))}
+              </div>
+            </div>
+          </Stack>
+          <ButtonLink icon="flashalt" to={`/music/${data._meta.uid}`}>Mehr infos</ButtonLink>
+        </Stack>
+      </Container>
+      <div
+        style={{
+          backgroundImage: `url(${data.cover_photo.Blur.url})`
+        }}
+        className={`${styles.backdropImage} pin`}
+      >
+      </div>
+    </section>
   )
 }
 
