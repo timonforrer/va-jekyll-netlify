b0666769ae6f3aae0f2a05f53611383e3d5e6a5c
[thecubix] <[timon.forrer@gmail.com]>
2019-08-09 21:31:04 +0200

    Minor updates to improve responsiveness

--- src/templates/gig.jsx
+++ src/templates/gig.jsx
@@ -3,8 +3,10 @@ import { graphql } from 'gatsby'
 import { RichText } from 'prismic-reactjs'
 import Layout from '../components/layout'
 
-import styles from '../pages/videos.module.scss'
+import styles from './gig.module.scss'
+import wrapperStyles from '../pages/videos.module.scss'
 
+import AspectRatio from '../components/aspectratio'
 import Container from '../components/container'
 import Stack from '../components/stack'
 import ButtonLink from '../components/buttonlink'
@@ -13,7 +15,7 @@ const Gig = ({data}) => {
   const at = data.airtable.data
   const pm = data.prismic.allGigs.edges.length !== 0 ? data.prismic.allGigs.edges[0].node.body : null
   return (
-    <Layout className={styles.wrapper}>
+    <Layout className={wrapperStyles.wrapper}>
       <Container dense>
         <Stack extended>
           <h1>{at.name}</h1>
@@ -26,7 +28,13 @@ const Gig = ({data}) => {
                     case ('rich_text'):
                       return <Stack key={index}>{RichText.render(section.primary.content)}</Stack>
                     case ('embed'):
-                      return <div key={index} dangerouslySetInnerHTML={{ __html: section.primary.url.html }} />
+                      return (
+                        <div className={styles.fullWidth}>
+                          <AspectRatio>
+                            <div key={index} dangerouslySetInnerHTML={{ __html: section.primary.url.html }} />
+                          </AspectRatio>
+                        </div>
+                      )
                   }
                 })
               }
